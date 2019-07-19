87398b4a1950b847379d227807f9fe05c6e63534
Kana Tsutsui <k_tsutsui@aidem.co.jp>
2019-07-19 11:01:12 +0900

    ベースだけ完成

--- sample.js
+++ sample.js
@@ -2,21 +2,20 @@ window.onload = function(){
     var can = document.getElementById("canvas");
     var ctx = can.getContext("2d");
     var color = "#fff";
-
-    var count = 0;//アニメーションカウンター
-    var timer = setInterval(function(){
-        ctx.fillStyle="#fff";//消去時の色
-        ctx.clearRect(0,0,300,300);//消す
-        ctx.fillStyle = color;//塗りつぶし色を決定
-        ctx.fillRect(50,50,200,200);
-        count++;
-        if(count>200){
-            clearInterval(timer);
-        }
-    },100);
+    ctx.fillStyle = color;//塗りつぶし色を決定
+    ctx.fillRect(50,50,200,200);//塗りつぶし
 
 }
 
-function change(color){
+function change(){
+
+    var can = document.getElementById("canvas");
+    var ctx = can.getContext("2d");
+    var color = document.getElementById("color").value;
+    //ここは値が渡せてるかの確認用
+    //confirm(color);
+    
     ctx.fillStyle = color;
+    ctx.fillRect(50,50,200,200);
+    
 }
